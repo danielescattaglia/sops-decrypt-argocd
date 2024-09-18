213c9d241085ff5bb7b309ceb6373767e0369e74
Daniele Scattaglia <daniele.scattaglia@dinova.one>
2024-09-18 18:47:42 +0200

    first commit

---
+++ sops-decrypt/decrypt.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func manifestRequestHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/api/v1/getparams.execute" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+    }
+
+
+    fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+
+    http.HandleFunc("/api/v1/getparams.execute", manifestRequestHandler)
+
+    fmt.Println("Hello, World!")
+     if err := http.ListenAndServe(":4355", nil); err != nil {
+        log.Fatal(err)
+     }
+}
---
+++ sops-decrypt/go.mod
@@ -0,0 +1,3 @@
+module argocd-custom/sops-decrypt
+
+go 1.23.1
